845b38282068deaa597e2e9046ded22b4865e853
zubaira <zubair@dhis2.org>
2017-02-09 13:17:03 +0100

    minor

--- src/test/java/org/hisp/appstore/support/TestSupport.java
+++ src/test/java/org/hisp/appstore/support/TestSupport.java
@@ -1,6 +1,7 @@
 package org.hisp.appstore.support;
 
 import org.hisp.appstore.configuration.Dhis2AppStore;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,4 +15,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class TestSupport
 {
+    @Before
+    protected void before()
+    {
+        setUpTest();
+    }
+
+    protected void setUpTest()
+    {
+    }
 }
--- src/test/java/org/hisp/appstore/web/AppControllerTest.java
+++ src/test/java/org/hisp/appstore/web/AppControllerTest.java
@@ -1,8 +1,20 @@
 package org.hisp.appstore.web;
 
+import org.hisp.appstore.support.TestSupport;
+import org.junit.Test;
+
 /**
  * Created by zubair on 09.02.17.
  */
-public class AppControllerTest
+public class AppControllerTest extends TestSupport
 {
+    @Override
+    protected void setUpTest()
+    {
+    }
+
+    @Test
+    public void testGetAllApps()
+    {
+    }
 }
