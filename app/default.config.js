29d9e759065178dd1a337af288aac406abbe413a
Birk Johansson <birk@dhis2.org>
2018-11-01 13:23:12 +0100

    Update dhis versions up to 2.31

--- app/default.config.js
+++ app/default.config.js
@@ -1,35 +1,38 @@
 module.exports = {
     api: {
-        baseURL: "https://play.dhis2.org/appstore/api/",
-        redirectURL: "https://play.dhis2.org/appstore/user"
+        baseURL: 'https://play.dhis2.org/appstore/api/',
+        redirectURL: 'https://play.dhis2.org/appstore/user',
     },
     auth0: {
-        clientID: "BTJ3iwPLO6hDC5w7JYWPlGd6461VNu81",
-        domain: "dhis2.eu.auth0.com"
+        clientID: 'BTJ3iwPLO6hDC5w7JYWPlGd6461VNu81',
+        domain: 'dhis2.eu.auth0.com',
     },
     routes: {
-        baseAppName: "/appstore"
+        baseAppName: '/appstore',
     },
     ui: {
         dhisVersions: [
-            "2.28",
-            "2.27",
-            "2.26",
-            "2.25",
-            "2.24",
-            "2.23",
-            "2.22",
-            "2.21"
+            '2.31',
+            '2.30',
+            '2.29',
+            '2.28',
+            '2.27',
+            '2.26',
+            '2.25',
+            '2.24',
+            '2.23',
+            '2.22',
+            '2.21',
         ],
         appStatusToDisplayName: {
-            NOT_APPROVED: "Rejected",
-            PENDING: "Pending",
-            APPROVED: "Approved"
+            NOT_APPROVED: 'Rejected',
+            PENDING: 'Pending',
+            APPROVED: 'Approved',
         },
         appTypeToDisplayName: {
-            APP: "Standard",
-            DASHBOARD_WIDGET: "Dashboard",
-            TRACKER_DASHBOARD_WIDGET: "Tracker Dashboard"
-        }
-    }
+            APP: 'Standard',
+            DASHBOARD_WIDGET: 'Dashboard',
+            TRACKER_DASHBOARD_WIDGET: 'Tracker Dashboard',
+        },
+    },
 };
