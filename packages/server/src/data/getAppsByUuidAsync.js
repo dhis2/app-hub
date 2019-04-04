a530b574d7480161d42bb61e614f5a43757c3d0f
Erik Arenhill <erik@dhis2.org>
2019-04-04 13:56:29 +0200

    fix: added try-catch around database call

--- packages/server/src/data/getAppsByUuidAsync.js
+++ packages/server/src/data/getAppsByUuidAsync.js
@@ -20,11 +20,18 @@ module.exports = async (uuid, languageCode, dbConnection) => {
         throw new Error(`Missing parameter 'dbConnection'`)
     }
 
-    return dbConnection
-        .select()
-        .from('apps_view')
-        .where({
-            uuid,
-            'language_code': languageCode
-        })
+    try {
+        const apps = await dbConnection
+            .select()
+            .from('apps_view')
+            .where({
+                uuid,
+                'language_code': languageCode
+            })
+
+        return apps
+    } catch ( err ) {
+        throw new Error(`Could not get apps with uuid: ${uuid} and languageCode: ${languageCode}`)
+    }
+
 }
