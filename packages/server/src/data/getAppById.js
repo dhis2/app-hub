10acd696b4108fe4fd5eed4fc10150d8fce5f658
Erik Arenhill <erik@dhis2.org>
2019-04-25 21:47:38 +0200

    fix: made it work as expected, and return all rows from apps_view

--- packages/server/src/data/getAppById.js
+++ packages/server/src/data/getAppById.js
@@ -1,13 +1,18 @@
 /**
  * Fetch an app based on its database id
  *
- * @param {number} id id for the app to fetch
+ * @param {number} appId id for the app to fetch
+ * @param {number} languageCode language to fetch
  * @param {*} knex db instance
  * @param {Promise}
  */
-const getAppById = async (appId, knex) => {
+const getAppById = async (appId, languageCode, knex) => {
     if (!appId) {
         throw new Error(`Missing parameter 'appId'`)
+    }
+
+    if (!languageCode) {
+        throw new Error(`Missing parameter 'languageCode'`)
     }
 
     if (!knex) {
@@ -15,13 +20,16 @@ const getAppById = async (appId, knex) => {
     }
 
     try {
-        const [app] = await knex('apps')
+        const appRows = await knex('apps_view')
             .select()
-            .where('id', appId)
+            .where({
+                app_id: appId,
+                language_code: languageCode,
+            })
 
-        return app
+        return appRows
     } catch (err) {
-        throw new Error(`Could not get app with id: ${appId}`)
+        throw new Error(`Could not get app with id: ${appId}. ${err.message}`)
     }
 }
 
