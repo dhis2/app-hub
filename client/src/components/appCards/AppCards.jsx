edf084c6931e855472eda6f8fbfe199ce3bdce08
Erik Arenhill <erik@dhis2.org>
2019-10-22 14:19:33 +0200

    feat: (WIP) add filter/popover for channels

--- client/src/components/appCards/AppCards.jsx
+++ client/src/components/appCards/AppCards.jsx
@@ -3,7 +3,14 @@ import { connect } from 'react-redux'
 import AppCardItem from './AppCardItem'
 import Grid from '../../material/Grid/Grid'
 import Col from '../../material/Grid/Col'
-import { loadAllApps, loadApprovedApps } from '../../actions/actionCreators'
+import Popover from 'material-ui/Popover'
+import IconButton from 'material-ui/IconButton'
+import FontIcon from 'material-ui/FontIcon'
+import {
+    loadAllApps,
+    loadApprovedApps,
+    loadChannels,
+} from '../../actions/actionCreators'
 import {
     TextFilter,
     filterApp,
@@ -20,8 +27,25 @@ import { FadeAnimation, FadeAnimationList } from '../utils/Animate'
 import '../../styles/utils/animations.scss'
 import Theme from '../../styles/theme'
 class AppCards extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            channelFilterOpen: false,
+            channelFilterAnchorEl: null,
+        }
+    }
+
     componentDidMount() {
         this.props.loadApps()
+        this.props.loadChannels()
+    }
+
+    handleOpenFilters(e) {
+        this.setState({
+            ...this.state,
+            channelFilterOpen: !this.state.channelFilterOpen,
+            channelFilterAnchorEl: e.currentTarget,
+        })
     }
 
     render() {
@@ -47,7 +71,10 @@ class AppCards extends Component {
                 textAlign: 'center',
             },
         }
-        const { loading, loaded, error, byId: cards } = this.props.appList
+        const { loaded, error, byId: cards } = this.props.appList
+        const loading =
+            this.props.appList.loading || this.props.channels.loading
+
         const loadOrErr = loading || error
         const searchFilter = this.props.appSearchFilter
             ? this.props.appSearchFilter.values.searchFilter
@@ -105,6 +132,38 @@ class AppCards extends Component {
                                 ]}
                             />
                         </ToolbarGroup>
+                        <ToolbarGroup>
+                            <IconButton
+                                onClick={this.handleOpenFilters.bind(this)}
+                            >
+                                <FontIcon className="material-icons">
+                                    filter_list
+                                </FontIcon>
+                            </IconButton>
+                            <Popover
+                                open={this.state.channelFilterOpen}
+                                anchorEl={this.state.channelFilterAnchorEl}
+                                style={{ width: '200px' }}
+                                onRequestClose={r =>
+                                    this.setState({ channelFilterOpen: false })
+                                }
+                            >
+                                <div style={{ padding: '10px' }}>
+                                    <h3>Channel</h3>
+                                    <SelectFilter
+                                        renderAllToggle
+                                        form="releaseChannel"
+                                        filters={this.props.channels.channels.map(
+                                            c => ({
+                                                label: c.name,
+                                                toggled: false,
+                                                value: c.id,
+                                            })
+                                        )}
+                                    />
+                                </div>
+                            </Popover>
+                        </ToolbarGroup>
                     </SubHeader>
                 </Col>
                 <Col span={12}>
@@ -135,11 +194,15 @@ const mapStateToProps = state => ({
     appList: state.appsList,
     filters: state.form.appTypeFilter,
     appSearchFilter: state.form.searchFilter,
+    channels: state.channels,
 })
 
 const mapDispatchToProps = dispatch => ({
     loadApps() {
         dispatch(loadApprovedApps())
+    },
+    loadChannels() {
+        dispatch(loadChannels())
     },
 })
 
