eac63cef1c58188a39e3d511ecb86b829c69743b
Erik Arenhill <erik@dhis2.org>
2019-11-05 13:39:29 +0100

    fix: parse port to number for comparsion

--- src/utils/getServerUrl.js
+++ src/utils/getServerUrl.js
@@ -6,13 +6,19 @@ const getServerUrl = request => {
     const protocol =
         request.headers['x-forwarded-proto'] || request.server.info.protocol
     const host = request.headers['x-forwarded-host'] || request.info.hostname
-    const port = request.headers['x-forwarded-port'] || request.server.info.port
+
+    //port is of type string from headers but an integer from request.server.info.port
+    const port = +(
+        request.headers['x-forwarded-port'] || request.server.info.port
+    )
 
     let portToUseInUrl = ''
+
+    //only add port to the url if it differs from the protocol standard port 80 vs 443 to get prettier urls
     if (
-        port &&
+        !isNaN(port) &&
         ((protocol === 'https' && port !== 443) ||
-            (protocol == 'http' && port !== 80))
+            (protocol === 'http' && port !== 80))
     ) {
         portToUseInUrl = `:${port}`
     }
