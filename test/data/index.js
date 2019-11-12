24fab500c0c32772c95c227271027de4dbec1f80
Erik Arenhill <erik@dhis2.org>
2019-11-12 11:53:31 +0100

    test: add tests for data/getAppById

--- test/data/index.js
+++ test/data/index.js
@@ -641,3 +641,28 @@ describe('@data::getAppDeveloperId', () => {
         expect(devId).to.be.false()
     })
 })
+
+describe('@data::getAppById', () => {
+    const getAppById = require('../../src/data/getAppById')
+
+    it('should require the appId parameter', async () => {
+        await expect(getAppById(undefined, 'en', db)).to.reject(
+            Error,
+            `Missing parameter 'appId'`
+        )
+    })
+
+    it('should require the languageCode parameter', async () => {
+        await expect(getAppById(1, undefined, db)).to.reject(
+            Error,
+            `Missing parameter 'languageCode'`
+        )
+    })
+
+    it('should require the db connection parameter knex', async () => {
+        await expect(getAppById(1, 'en', undefined)).to.reject(
+            Error,
+            `Missing parameter 'knex'`
+        )
+    })
+})
