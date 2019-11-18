d4eea8e3bf8ffa58e8fe6e825b14ffe6d2972daf
Erik Arenhill <erik@dhis2.org>
2019-11-18 20:35:42 +0100

    test: make sure renameChannel works as intended

--- test/data/channels.js
+++ test/data/channels.js
@@ -7,8 +7,9 @@ const { expect } = require('@hapi/code')
 const knexConfig = require('../../knexfile')
 const db = require('knex')(knexConfig)
 
-describe('@data::createChannel', () => {
+describe('@data::channels', () => {
     const createChannel = require('../../src/data/createChannel')
+    const renameChannel = require('../../src/data/renameChannel')
 
     it('should create a channel named Test', async () => {
         const transaction = await db.transaction()
@@ -23,6 +24,27 @@ describe('@data::createChannel', () => {
         expect(channel.id)
             .to.be.a.number()
             .greaterThan(0)
+
+        transaction.rollback()
+    })
+
+    it('should create a channel named Foo and then change name of it to Bar', async () => {
+        const transaction = await db.transaction()
+
+        const channel = await createChannel({ name: 'Foo' }, db, transaction)
+
+        expect(channel).to.not.be.null()
+        expect(channel.uuid).to.exist()
+        expect(channel.id).to.exist()
+        expect(channel.name).to.equal('Foo')
+
+        const changedChannel = await renameChannel({ name: 'Foo', newName: 'Bar'}, db, transaction)
+
+        expect(changedChannel.id).to.equal(channel.id)
+        expect(changedChannel.uuid).to.equal(channel.uuid)
+        expect(changedChannel.name).to.equal('Bar')
+
+        transaction.rollback()
     })
 
     it('should require the parameter name to be passed', async () => {
