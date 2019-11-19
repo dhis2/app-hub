b75a0e1154422068162306fb02ec5f5c6941a0d5
Erik Arenhill <erik@dhis2.org>
2019-11-19 07:41:34 +0100

    test: make sure get channels returns items

--- test/routes/v2/channels.js
+++ test/routes/v2/channels.js
@@ -31,4 +31,19 @@ describe('api/v2/channels', () => {
 
         expect(response.statusCode).to.equal(401)
     })
+
+    it('should return some channels with status code 200', async () => {
+        const response = await server.inject({
+            method: 'GET',
+            url: '/api/v2/channels',
+        })
+
+        expect(response.statusCode).to.equal(200)
+
+        const json = JSON.parse(response.payload)
+        expect(json).to.be.an.array()
+        expect(json.length)
+            .to.be.a.number()
+            .and.greaterThan(0)
+    })
 })
