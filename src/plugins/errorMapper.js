2cc55e25d20b25805c840b0a41cf3fd6af35247c
Birk Johansson <birk@dhis2.org>
2019-12-13 17:10:33 +0100

    feat(plugin): error-mapper
    fix(errormapper): check for constructor name instead of instanceof
    
    test(errormapper): more tests for errormapper-plugin

---
+++ src/plugins/errorMapper.js
@@ -0,0 +1,65 @@
+const { NotFoundError } = require('../utils/errors')
+const Boom = require('@hapi/boom')
+const {
+    UniqueViolationError,
+    ForeignKeyViolationError,
+    NotNullViolationError,
+    CheckViolationError,
+    DBError,
+    DataError,
+    ConstraintViolationError,
+    wrapError,
+} = require('db-errors')
+
+const dbErrorMap = {
+    badRequest: [
+        CheckViolationError,
+        ConstraintViolationError,
+        DataError,
+        ForeignKeyViolationError,
+        NotNullViolationError,
+    ],
+    conflict: [UniqueViolationError],
+    notFound: [NotFoundError],
+    internal: [DBError],
+}
+
+const onPreResponseHandler = function(request, h) {
+    const { response: error } = request
+    // not error or joi-error - ignore
+    // By default validation errors thrown in handler are transformed to 500-error, as they are not Boom-errors.
+    // This makes sense, as internal validation errors (outside validation-handlers) are developer errors
+    if (!error.isBoom || error.isJoi) {
+        return h.continue
+    }
+    throw mapError(error, this.options)
+}
+
+const mapError = (error, options) => {
+    const wrappedError = wrapError(error)
+    for (const key in dbErrorMap) {
+        const httpError = dbErrorMap[key].find(
+            e => wrappedError.constructor.name === e.name
+        )
+        if (httpError) {
+            return Boom[key](options.preserveMessage ? error.message : null)
+        }
+    }
+    return error
+}
+
+const defaultOptions = {
+    preserveMessage: false,
+}
+
+const errorPlugin = {
+    name: 'ErrorPlugin',
+    register: async (server, options = defaultOptions) => {
+        server.bind({
+            options,
+        })
+        server.ext('onPreResponse', onPreResponseHandler)
+    },
+}
+
+module.exports = errorPlugin
