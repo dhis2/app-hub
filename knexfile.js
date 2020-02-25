2c6bb4a0957f2ee42c6a6c8c3a3e8514787d0c2e
Erik Arenhill <erik@dhis2.org>
2020-02-25 12:00:59 +0100

    feat: add support connect to db on other port

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,7 @@ const debug = require('debug')('apphub:server:knexconfig')
 debug(`process.env.RDS_HOSTNAME: ${process.env.RDS_HOSTNAME}`)
 debug(`process.env.RDS_USERNAME: ${process.env.RDS_USERNAME}`)
 debug(`process.env.RDS_DB_NAME: ${process.env.RDS_DB_NAME}`)
+debug(`process.env.RDS_DB_PORT: ${process.env.RDS_DB_PORT}`)
 
 module.exports = {
     client: 'pg',
@@ -13,6 +14,7 @@ module.exports = {
         user: process.env.RDS_USERNAME,
         password: process.env.RDS_PASSWORD,
         database: process.env.RDS_DB_NAME,
+        port: process.env.RDS_DB_PORT || 5432, //fallback to postgres port as that's what we normally use
     },
     searchPath: ['knex', 'public', 'postgres', 'apphub'],
     pool: {
