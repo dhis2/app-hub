20e9402ab0f99d445a844a6f314a1b2a268b466a
Erik <erik@dhis2.org>
2020-03-04 11:26:10 +0100

    feat: insert api user on server boot

--- server/src/main.js
+++ server/src/main.js
@@ -8,6 +8,7 @@ const { createChannel } = require('./data/index.js')
 const { migrate } = require('./server/migrate-database.js')
 const { init } = require('./server/init-server.js')
 const { config } = require('./server/env-config.js')
+const { createApiUser } = require('./server/create-api-user.js')
 
 debug('Using env: ', process.env.NODE_ENV)
 
@@ -26,6 +27,11 @@ migrate(knex)
     )
     .then(r => debug('Channel was created', r))
     .catch(r => debug('Channel probably exists, skipping', r))
+    .then(() => {
+        knex.transaction(trx => createApiUser(knex, trx))
+    })
+    .then(() => debug('API User was created or already existed'))
+    .catch(r => debug('API User probably exists, skipping', r))
     .then(() => init(knex, config))
     .catch(err => {
         debug('The server failed to start.\n', err)
