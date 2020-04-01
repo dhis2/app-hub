1327ee89e29693fe053213ab86a44cd368bbaff8
Birk Johansson <birk@dhis2.org>
2020-04-01 14:51:09 +0200

    fix(filter): filter creation with renames

--- server/src/utils/Filter.js
+++ server/src/utils/Filter.js
@@ -5,6 +5,12 @@ const debug = require('debug')('apphub:server:utils:Filter')
 class Filters {
     /**
      *
+     * Encapsulates logic related to filtering. Uses a query-filter object that can be applied
+     * to a knex-query with applyAllToQuery() or applyOneToQuery()
+     *
+     * If renames are used, all interfacing field-names should use the 'original-name', that is the name before
+     * renames. This is so that all application-logic uses the original-name instead of the internal database column-name
+     *
      * @param {*} filters an object where the key is the field name of the filter, value is an object of shape
      * {
      *  value - the value of the filter,
@@ -15,25 +21,34 @@ class Filters {
      * @param {Object} options options object merged with the object passed to `applyToQuery`
      */
     constructor(filters = {}, { renameMap } = {}, options = {}) {
-        // filters before validation
-        this.originalFilters = filters
         this.options = options
         this.renameMap = renameMap || {} //map of renames, from -> to
         this.appliedFilters = new Set()
-        this.filters = filters
+        this.filters = filters // keys are before-renames
     }
 
     /**
+     * Parses an object with query-filters and creates an instance of Filters.
      *
-     * @param {*} filters an object with filters, where the key is the field name, value is a filter-string, like
+     * Note that this is not normally used in routes. That creation is done
+     * in queryFilter-plugin, and validation is done by hapi through normal query-validation.
+     * This can however be used as a helper to create an instance of Filters outside of the normal flow.
+     * Eg. in tests.
+     *
+     * @param {Object} filters an object with filters, where the key is the field name, value is a filter-string, like
      * `eq:DHIS2`.
      *
-     * @param {*} validate a joi validation schema
+     * @param {Object} creationOptions an object with keys
+     * @param {Joi.schema} creationOptions.validate a Joi-schema used to validate and transform the filters-object.
+     * @param {Joi.schema || Object} creationOptions.rename A Joi-schema or a rename-object where the keys are the from-name and values are the
+     * database column-name. If a Joi-schema it should have .rename() called on the schema. This is not used to validate,
+     * and is supported to act as a shorthand to pass in renames from an already defined Joi 'Model'-schema.
+     * @param {*} options a Joi-configuration passed to Joi.attempt() when validating
      *
      * @returns An instance of Filters, where the filters are validated/transformed using `validation`.
      */
-    static createFromQueryFilters(filters, validate, options) {
-        let result = {}
+    static createFromQueryFilters(filters, { validate, rename } = {}, options) {
+        let result = filters
         let renameMap = null
         Object.keys(filters).map(key => {
             try {
@@ -46,32 +61,34 @@ class Filters {
             return result
         })
         if (validate && Joi.isSchema(validate)) {
-            result = Joi.attempt(result, validate, {
-                convert: false,
-                stripUnnown: false,
+            result = Joi.attempt(filters, validate, {
+                convert: false, //don't convert, this is done above in parseFilterString
                 ...options,
             })
-            const renames = validate.describe().renames
-            if (renames) {
-                renameMap = renames.reduce((acc, curr) => {
-                    const { from, to } = curr
-                    acc[from] = to
-                    return acc
-                }, {})
-            }
-        } else {
-            renameMap = validate
         }
-
+        if (rename) {
+            if (Joi.isSchema(rename)) {
+                const renames = rename.describe().renames
+                if (renames) {
+                    renameMap = renames.reduce((acc, curr) => {
+                        const { from, to } = curr
+                        acc[from] = to
+                        return acc
+                    }, {})
+                }
+            } else {
+                renameMap = rename
+            }
+        }
         return new Filters(result, { renameMap }, options)
     }
 
-    getFilter(field) {
-        return this.filters[field]
+    getFilter(fieldName) {
+        return this.filters[fieldName]
     }
 
-    getFilterColumn(field) {
-        return this.renameMap[field] || field
+    getFilterColumn(fieldName) {
+        return this.renameMap[fieldName] || fieldName
     }
 
     isEmpty() {
@@ -82,11 +99,18 @@ class Filters {
         return !this.isEmpty()
     }
 
-    applyOneToQuery(query, field, options) {
-        const colName = this.getFilterColumn(field)
-        const filter = this.filter[field]
+    /**
+     * Applies the filter
+     * @param {*} query
+     * @param {*} fieldName
+     * @param {*} options
+     */
+
+    applyOneToQuery(query, fieldName, options) {
+        const colName = this.getFilterColumn(fieldName)
+        const filter = this.filter[fieldName]
         if (filter) {
-            this.markApplied(field)
+            this.markApplied(fieldName)
             query.where(
                 options.tableName ? `${options.tableName}.${colName}` : colName,
                 toSQLOperator(filter.operator),
@@ -99,13 +123,21 @@ class Filters {
         }
     }
 
+    /**
+     * Marks the filter as applied. Can be useful when creating a custom query,
+     * and you don't want the filter to be applied when running applyAllToQuery().
+     *
+     * @param {*} field field to mark as applied. The field is ignored
+     */
     markApplied(field) {
         this.appliedFilters.add(field)
     }
 
     /**
+     * Applies all internal filters to the knex-query.
+     * The query might fail if the filter-value is incompatible with the database-column type.
      *
-     * @param {*} query knex query-builder-instance to add a where-clause to.
+     * @param {*} query knex query-builder-instance to add a where-clause(s) to.
      * @param {*} options options object, merged with the one given to the Filters instance.
      * @param {string} options.tableName
      */
--- server/test/utils/filter.js
+++ server/test/utils/filter.js
@@ -54,7 +54,9 @@ describe('Filters', () => {
                 name: Joi.filter(),
             })
 
-            const filters = Filters.createFromQueryFilters(queryFilters, schema)
+            const filters = Filters.createFromQueryFilters(queryFilters, {
+                validate: schema,
+            })
 
             expect(filters).to.be.instanceOf(Filters)
             expect(filters.filters.name).to.be.equal({
@@ -72,22 +74,49 @@ describe('Filters', () => {
             })
 
             const throws = () => {
-                Filters.createFromQueryFilters(queryFilters, schema)
+                Filters.createFromQueryFilters(queryFilters, {
+                    validate: schema,
+                })
             }
             expect(throws).to.throw(ValidationError, '"name" must be a number')
         })
 
-        it('should support rename of keys', () => {
+        it('should support rename of keys by Joi-schema', () => {
             const queryFilters = {
                 owner: 'eq:58262f57-4f38-45c5-a3c2-9e30ab3ba2da',
             }
             const schema = Joi.object({
-                created_by_user_id: Joi.filter(Joi.string().guid()),
+                owner: Joi.filter(Joi.string().guid()),
+            })
+            const renameSchema = Joi.object({
+                owner: Joi.string(),
             }).rename('owner', 'created_by_user_id')
 
-            const filters = Filters.createFromQueryFilters(queryFilters, schema)
-            expect(filters.filters.created_by_user_id).to.be.an.object()
-            expect(filters.filters.owner).to.be.undefined()
+            const filters = Filters.createFromQueryFilters(queryFilters, {
+                validate: schema,
+                rename: renameSchema,
+            })
+            expect(filters.getFilter('owner')).to.be.an.object()
+            expect(filters.getFilterColumn('owner')).to.be.equal(
+                'created_by_user_id'
+            )
+        })
+
+        it('should support rename of keys by rename-object', () => {
+            const queryFilters = {
+                owner: 'eq:58262f57-4f38-45c5-a3c2-9e30ab3ba2da',
+            }
+
+            const rename = {
+                owner: 'created_by_user_id',
+            }
+            const filters = Filters.createFromQueryFilters(queryFilters, {
+                rename,
+            })
+            expect(filters.getFilter('owner')).to.be.an.object()
+            expect(filters.getFilterColumn('owner')).to.be.equal(
+                'created_by_user_id'
+            )
         })
     })
 })
