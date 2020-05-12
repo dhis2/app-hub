ae3c403a3e8ba034affd21f3780275191c07cccf
Birk Johansson <birk@dhis2.org>
2020-05-12 12:59:08 +0200

    refactor: use debug for logging

--- client/src/app-hub.js
+++ client/src/app-hub.js
@@ -4,8 +4,11 @@ import AppHub from './components/AppHub.jsx'
 import 'whatwg-fetch'
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
+import Debug from 'debug'
 
-console.log(`App Hub, version ${__APP_INFO__.version}`) //eslint-disable-line no-undef
-console.log(`Built: ${new Date(__APP_INFO__.built).toString()}`) //eslint-disable-line no-undef
+const debug = Debug('apphub:init')
+
+debug(`App Hub, version ${__APP_INFO__.version}`) //eslint-disable-line no-undef
+debug(`Built: ${new Date(__APP_INFO__.built).toString()}`) //eslint-disable-line no-undef
 
 render(<AppHub />, document.getElementById('appHub'))
