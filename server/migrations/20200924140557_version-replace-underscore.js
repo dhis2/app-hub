b22fa9d6d3546f355037ff42f59c6b5d5a63e31b
Birk Johansson <birk@dhis2.org>
2020-09-24 18:51:38 +0200

    fix(migration): remove down migration

--- server/migrations/20200924140557_version-replace-underscore.js
+++ server/migrations/20200924140557_version-replace-underscore.js
@@ -1,18 +1,12 @@
-
 exports.up = function(knex) {
-
     return knex.raw(
         `update app_version
         set version = replace(version, '_', '.')
         where version like '%$_%' ESCAPE '$'
-    `)
-};
+    `
+    )
+}
 
 exports.down = function(knex) {
-
-    return knex.raw(
-        `update app_version
-        set version = replace(version, '.', '_')
-        where version like '%$.%' ESCAPE '$'
-    `)
-};
+    return Promise.resolve()
+}
