547affa0f833d3db639bffea8e75b03ec1ea1981
Birk Johansson <birk@dhis2.org>
2020-10-13 12:51:28 +0200

    feat(migrations): add organisation email

---
+++ server/migrations/20201006112837_add_organisation_email.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+    return knex.schema.table('organisation', table => {
+        table.string('email')
+    })
+}
+
+exports.down = function(knex) {
+    return knex.schema.table('organisation', table => {
+        table.dropColumn('email')
+    })
+}
