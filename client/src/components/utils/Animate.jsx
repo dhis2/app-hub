873747567e1b629b0fbba3cb937436f4c8684fc8
Birk Johansson <birk@dhis2.org>
2020-10-30 15:42:03 +0100

    fix: cleanup old spinner

--- client/src/components/utils/Animate.jsx
+++ client/src/components/utils/Animate.jsx
@@ -1,9 +1,9 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from 'prop-types'
+import React from 'react'
 import TransitionGroup from 'react-transition-group/TransitionGroup'
 import Transition from 'react-transition-group/Transition'
 import CSSTransition from 'react-transition-group/CSSTransition'
-import '../../styles/utils/animations.scss'
+import '../../styles/utils/animations.css'
 
 const duration = 300
 
