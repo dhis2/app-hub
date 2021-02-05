66f6e2628fc8e79005ade012d353863bfe767b58
Médi-Rémi Hashim <mediremi@gmail.com>
2021-02-05 13:03:23 +0000

    test(server): add /v2/apps tests

--- server/test/routes/index.js
+++ server/test/routes/index.js
@@ -164,14 +164,108 @@ describe('Get all published apps [v2]', () => {
         await server.stop()
     })
 
-    it('should just return a 501 not implemented for the moment being', async () => {
+    it('should return some test-apps from seeded db', async () => {
         const injectOptions = {
             method: 'GET',
             url: '/api/v2/apps',
         }
 
         const response = await server.inject(injectOptions)
-        expect(response.statusCode).to.equal(501)
+        expect(response.statusCode).to.equal(200)
+
+        const { apps } = JSON.parse(response.payload)
+        expect(apps).to.not.be.empty()
+
+        const approvedApps = apps.filter(app => app.status === 'APPROVED')
+        expect(approvedApps.length).to.be.equal(apps.length)
+
+        const whoApp = apps.filter(app => app.name === 'A nice app by WHO')
+
+        expect(whoApp).to.be.array()
+
+        expect(whoApp[0].developer.email).to.be.equal('erik@dhis2.org')
+        expect(whoApp[0].developer.organisation).to.be.equal(
+            'World Health Organization'
+        )
+        expect(whoApp[0].developer.name).to.be.equal('Erik Arenhill')
+
+        expect(whoApp[0].versions[0].version).to.be.equal('1.0')
+        expect(whoApp[0].versions[0].demoUrl).to.be.equal(
+            'https://play.dhis2.org/2.30/api/apps/Immunization-analysis/index.html#!/report'
+        )
+        expect(whoApp[0].versions[0].downloadUrl).to.be.equal(
+            'http://localhost:3000/api/v1/apps/download/world-health-organization/a-nice-app-by-who_1.0.zip'
+        )
+
+        expect(whoApp[0].sourceUrl).to.be.equal(
+            'https://github.com/dhis2/who-immunization-analysis-app/'
+        )
+    })
+
+    it('should only return apps from the Stable channel', async () => {
+        const injectOptions = {
+            method: 'GET',
+            url: '/api/v2/apps?channel=Stable',
+        }
+
+        const response = await server.inject(injectOptions)
+        expect(response.statusCode).to.equal(200)
+
+        const { apps } = JSON.parse(response.payload)
+        expect(apps).to.not.be.empty()
+        expect(apps).to.be.array()
+
+        const versions = flatten(apps.map(app => app.versions))
+        const filteredVersions = versions.filter(
+            version => version.channel === 'Stable'
+        )
+        expect(filteredVersions.length).to.equal(versions.length)
+    })
+
+    it('should only return apps from the Stable channel supporting version 2.27', async () => {
+        const injectOptions = {
+            method: 'GET',
+            url: '/api/v2/apps?channel=Stable&dhis_version=2.27',
+        }
+
+        const response = await server.inject(injectOptions)
+        expect(response.statusCode).to.equal(200)
+
+        const { apps } = JSON.parse(response.payload)
+        expect(apps).to.not.be.empty()
+        expect(apps).to.be.array()
+
+        const versions = flatten(apps.map(app => app.versions))
+
+        const filteredVersions = versions.filter(
+            version => version.channel === 'Stable'
+        )
+        expect(filteredVersions.length).to.equal(versions.length)
+
+        //the 'a nice app by who' version 1.0 with support for 2.27
+        expect(filteredVersions[0].maxDhisVersion).to.equal(null)
+        expect(filteredVersions[0].minDhisVersion).to.equal('2.27')
+    })
+
+    it('should only return apps supporting version 2.27', async () => {
+        const injectOptions = {
+            method: 'GET',
+            url: '/api/v2/apps?channel=All&dhis_version=2.27',
+        }
+
+        const response = await server.inject(injectOptions)
+        expect(response.statusCode).to.equal(200)
+
+        const { apps } = JSON.parse(response.payload)
+        expect(apps).to.not.be.empty()
+        expect(apps).to.be.array()
+
+        const versions = flatten(apps.map(app => app.versions))
+        expect(versions.length).to.equal(1)
+
+        //the 'a nice app by who' version 1.0 with support for 2.27
+        expect(versions[0].maxDhisVersion).to.equal(null)
+        expect(versions[0].minDhisVersion).to.equal('2.27')
     })
 })
 
