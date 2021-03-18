43d5f34c2cb964116617f8d42965f8a2b2fdbe8e
Birk Johansson <birk@dhis2.org>
2021-03-18 17:48:57 +0100

    fix: add warning prop to Noteblock

--- client/src/components/utils/NoteBlock.jsx
+++ client/src/components/utils/NoteBlock.jsx
@@ -23,9 +23,24 @@ const styles = {
         padding: '4px',
         marginRight: '8px',
     },
+    warningContainerStyle: {
+        backgroundColor: '#ffecb3',
+        border: '1px solid #ffe082',
+    },
+    criticalContainerStyle: {
+        backgroundColor: '#e57373',
+        border: '1px solid #f44336',
+    },
 }
 export const NoteBlock = props => (
-    <div style={{ ...styles.containerStyle, ...props.styles }}>
+    <div
+        style={{
+            ...styles.containerStyle,
+            ...props.styles,
+            ...(props.warning && styles.warningContainerStyle),
+            ...(props.critical && styles.criticalContainerStyle),
+        }}
+    >
         <div style={styles.iconHeader}>
             <FontIcon style={styles.iconStyle} className="material-icons">
                 {props.icon || 'info_outline'}
@@ -42,6 +57,7 @@ NoteBlock.propTypes = {
     children: PropTypes.node,
     header: PropTypes.node,
     icon: PropTypes.string,
+    warning: PropTypes.bool,
 }
 
 export default NoteBlock
