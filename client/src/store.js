2d79ec3b172c35740fdfff59b4306477682d1435
Médi-Rémi Hashim <mediremi@gmail.com>
2021-04-08 19:49:32 +0100

    chore: remove unused reducers and actions

--- client/src/store.js
+++ client/src/store.js
@@ -2,17 +2,10 @@ import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import { createLogger } from 'redux-logger'
 import { createEpicMiddleware } from 'redux-observable'
 import Epics from './actions/epics'
-import appListReducer from './reducers/appListReducer'
-import channelReducer from './reducers/channelReducer'
-import dialogReducer from './reducers/dialogReducer'
-import formReducer from './reducers/formReducer'
-import organisationReducer from './reducers/organisationReducer'
-import snackbarReducer from './reducers/snackbarReducer'
 import userReducer from './reducers/userReducer'
-import optimisticMiddleware from './store/ReduxOptimisticMiddleware'
 
 const epicMiddleware = createEpicMiddleware()
-const middlewares = [optimisticMiddleware, epicMiddleware]
+const middlewares = [epicMiddleware]
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(
@@ -24,13 +17,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const reducer = combineReducers({
-    appsList: appListReducer,
     user: userReducer,
-    dialog: dialogReducer,
-    snackbar: snackbarReducer,
-    form: formReducer,
-    channels: channelReducer,
-    organisations: organisationReducer,
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
