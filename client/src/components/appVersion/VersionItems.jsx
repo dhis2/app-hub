338e301e7092c20b82ffffd37ede3425130ed181
Médi-Rémi Hashim <mediremi@gmail.com>
2021-04-08 18:17:26 +0100

    refactor: remove unused components

--- client/src/components/appVersion/VersionItems.jsx
+++
@@ -1,9 +0,0 @@
-import MenuItem from 'material-ui/MenuItem'
-import React from 'react'
-import config from '../../../config'
-
-export const VersionMenuItems = config.ui.dhisVersions.map(version => (
-    <MenuItem key={version} value={version} primaryText={version} />
-))
-
-export default VersionMenuItems
