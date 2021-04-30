ca3ee4855a419fa7c8a53b292427bd6b3f90c8d0
Médi-Rémi Hashim <mediremi@gmail.com>
2021-04-30 10:20:30 +0100

    fix: don't use deprecated punycode core module

--- server/src/utils/slugify.js
+++ server/src/utils/slugify.js
@@ -1,4 +1,7 @@
-const punycode = require('punycode')
+// The punycode core module is deprecated but userland modules don't hide core
+// modules,so we have to add a forward slash at the end of the import in order
+// to specify the userland module.
+const punycode = require('punycode/')
 const slugify = require('slugify')
 
 exports.slugify = string => slugify(punycode.encode(string), { lower: true })
