c6013faeed959cc246e6c1bb937de159be36e56d
Médi-Rémi Hashim <mediremi@gmail.com>
2021-05-03 21:32:24 +0100

    feat: add favicons

--- server/src/plugins/staticFrontendRoutes.js
+++ server/src/plugins/staticFrontendRoutes.js
@@ -1,22 +1,31 @@
 const path = require('path')
 
+const staticDir = path.join(__dirname, '..', '..', 'static')
+
+const faviconRoutes = [
+    'apple-touch-icon.png',
+    'favicon-32x32.png',
+    'favicon-16x16.png',
+].map(file => ({
+    method: 'GET',
+    path: `/${file}`,
+    handler: {
+        file: path.join(__dirname, '..', '..', 'public', file),
+    },
+}))
+
 const staticFrontendRoutes = {
     name: 'DHIS2 App Hub Frontend',
-    register: function(server, options) {
+    register: function(server) {
         //Temporary route to serve frontend static build until we've flattened the project structure
         server.route([
+            ...faviconRoutes,
             {
                 method: 'GET',
                 path: '/assets/{param*}',
                 handler: {
                     directory: {
-                        path: path.join(
-                            __dirname,
-                            '..',
-                            '..',
-                            'static',
-                            'assets'
-                        ),
+                        path: path.join(staticDir, 'assets'),
                     },
                 },
             },
@@ -25,7 +34,7 @@ const staticFrontendRoutes = {
                 path: '/js/{param*}',
                 handler: {
                     directory: {
-                        path: path.join(__dirname, '..', '..', 'static', 'js'),
+                        path: path.join(staticDir, 'js'),
                     },
                 },
             },
@@ -33,13 +42,7 @@ const staticFrontendRoutes = {
                 method: 'GET',
                 path: '/{param*}',
                 handler: {
-                    file: path.join(
-                        __dirname,
-                        '..',
-                        '..',
-                        'static',
-                        'index.html'
-                    ),
+                    file: path.join(staticDir, 'index.html'),
                 },
             },
         ])
