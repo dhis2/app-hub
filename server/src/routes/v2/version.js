dbc2732dba8eb63b93cf9b5731087405da5a65e7
Médi-Rémi Hashim <4295266+mediremi@users.noreply.github.com>
2021-05-07 09:29:59 +0100

    feat: add version API (#473)

---
+++ server/src/routes/v2/version.js
@@ -0,0 +1,9 @@
+const { version } = require('../../../../package.json')
+
+module.exports = [
+    {
+        method: 'GET',
+        path: '/v2/version',
+        handler: () => ({ version }),
+    },
+]
