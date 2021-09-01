21d42131b3fb16eed42e1830970b0eea5eb14261
Birk Johansson <birk@dhis2.org>
2021-09-01 14:36:02 +0200

    chore: upgrade dev-deps

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,13 +35,12 @@ const webpackConfig = {
                     {
                         loader: 'css-loader',
                         options: {
-                            modules: true,
-                            // TODO: This option has moved to inside the
-                            // 'modules' config for later versions of css-loader
-                            localIdentName:
-                                nodeEnv === 'development'
-                                    ? '[path][name]__[local]'
-                                    : '[hash:base64]',
+                            modules: {
+                                localIdentName:
+                                    nodeEnv === 'development'
+                                        ? '[path][name]__[local]'
+                                        : '[hash:base64]',
+                            },
                         },
                     },
                 ],
