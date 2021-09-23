561d46619592cf8d875d579679ccdbe95ac36bc8
Birk Johansson <birk@dhis2.org>
2021-09-24 00:53:46 +0200

    fix: add laggySWRmiddleware

--- client/src/api/utils.js
+++ client/src/api/utils.js
@@ -1,3 +1,5 @@
+import { useRef, useEffect, useCallback } from 'react'
+
 const encodeQueryParameter = param => {
     if (Array.isArray(param)) {
         return param.map(encodeQueryParameter).join(',')
@@ -41,3 +43,42 @@ export const queryParametersToQueryString = params =>
                 `${encodeURIComponent(key)}=${encodeQueryParameter(value)}`
         )
         .join('&')
+
+// This is a SWR middleware for keeping the data even if key changes.
+// See https://swr.vercel.app/docs/middleware#keep-previous-result
+export const laggySWRMiddleware = useSWRNext => {
+    return (key, fetcher, config) => {
+        // Use a ref to store previous returned data
+        const laggyDataRef = useRef()
+
+        // Actual SWR hook.
+        const swr = useSWRNext(key, fetcher, config)
+
+        useEffect(() => {
+            // Update ref if data is not undefined
+            if (swr.data !== undefined) {
+                laggyDataRef.current = swr.data
+            }
+        }, [swr.data])
+
+        // Expose a method to clear the laggy data, if any
+        const resetLaggy = useCallback(() => {
+            laggyDataRef.current = undefined
+        }, [])
+
+        // Fallback to previous data if the current data is undefined
+        const dataOrLaggyData =
+            swr.data === undefined ? laggyDataRef.current : swr.data
+
+        // Is it showing previous data?
+        const isLagging =
+            swr.data === undefined && laggyDataRef.current !== undefined
+
+        // Also add a `isLagging` field to SWR.
+        return Object.assign({}, swr, {
+            data: dataOrLaggyData,
+            isLagging,
+            resetLaggy,
+        })
+    }
+}
