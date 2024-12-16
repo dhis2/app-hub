973ddbf66deffc186f62bf7e5745c3ca1c8e0409
Mozafar Haider <mozafar@dhis2.org>
2024-12-16 22:16:20 +0200

    refactor: remove unnecessary modal property

--- client/src/components/Versions/ChangeLogViewer.js
+++ client/src/components/Versions/ChangeLogViewer.js
@@ -1,21 +1,22 @@
-import { CircularLoader, Modal, ModalContent } from '@dhis2/ui'
+import { CenteredContent, CircularLoader, Modal, ModalContent } from '@dhis2/ui'
 import { useQuery } from 'src/api'
 
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 
 // ToDo: this is a just a placeholder for a  basic Changelog viewer
-const ChangeLogViewer = ({ appId, modalShown, onCloseChangelog }) => {
+const ChangeLogViewer = ({ appId, onCloseChangelog }) => {
     const { data, loading } = useQuery(`apps/${appId}/changelog`)
 
-    if (!modalShown) {
-        return null
-    }
     return (
         <Modal onClose={onCloseChangelog}>
             <ModalContent>
                 <div>
-                    {loading && <CircularLoader large />}
+                    {loading && (
+                        <CenteredContent>
+                            <CircularLoader large />
+                        </CenteredContent>
+                    )}
                     <ReactMarkdown>{data?.changelog}</ReactMarkdown>
                 </div>
             </ModalContent>
