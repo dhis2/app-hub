937e31cb5a256aa6550825bf6c506e13f0807e38
Mozafar Haider <mozafar@dhis2.org>
2025-07-25 12:58:45 +0100

    fix(DHIS2-19851): ensure that stable versions - not development - are shown as latest version

--- client/src/pages/AppView/LatestUpdates.js
+++ client/src/pages/AppView/LatestUpdates.js
@@ -1,19 +1,32 @@
+import PropTypes from 'prop-types'
 import ReactMarkdown from 'react-markdown'
 import { useHistory } from 'react-router-dom'
 import styles from './LatestUpdates.module.css'
 
-export const LatestUpdates = ({ changelog }) => {
+export const LatestUpdates = ({ versions, changelog }) => {
     const history = useHistory()
     if (!changelog) {
         return null
     }
+
+    const versionsToShow = changelog.filter(
+        ({ version }) =>
+            !!versions.find(
+                (v) => v.version === version && v.channel === 'stable'
+            )
+    )
+
+    if (versionsToShow?.length === 0) {
+        return null
+    }
+
     return (
         <div className={styles.latestUpdatesWrapper}>
             <div className={styles.latestUpdates}>
                 <h2 className={styles.latestUpdatesHeader}>Latest updates:</h2>
 
                 <ol className={styles.versionList}>
-                    {changelog?.slice(0, 3).map((version, i) => {
+                    {versionsToShow?.slice(0, 3).map((version, i) => {
                         return (
                             <li key={version.version}>
                                 <h3
@@ -43,4 +56,9 @@ export const LatestUpdates = ({ changelog }) => {
             </div>
         </div>
     )
+}
+
+LatestUpdates.propTypes = {
+    changelog: PropTypes.object,
+    versions: PropTypes.array,
 }
